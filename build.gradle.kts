f5da0c04ddc4c1803971b98d0690287bd86330f6
Marc Philipp <mail@marcphilipp.de>
2019-02-02 11:24:24 +0100

    Release 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ buildScan {
 }
 
 group = "de.marcphilipp.gradle"
-version = "0.1.1"
+version = "0.2.0"
 
 val readableName = "Nexus Publish Plugin"
 description = "Gradle Plugin for publishing to Nexus repositories"
