67fd0c9c1da3794b34c5514739b26982151b2008
Marc Philipp <mail@marcphilipp.de>
2021-02-10 08:40:14 +0100

    Change version to 1.0.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 }
 
 group = "io.github.gradle-nexus"
-version = "0.1.0-SNAPSHOT"
+version = "1.0.0-SNAPSHOT"
 
 val readableName = "Nexus Publish Plugin"
 description = "Gradle Plugin for publishing, closing, and releasing to Nexus staging repositories"
