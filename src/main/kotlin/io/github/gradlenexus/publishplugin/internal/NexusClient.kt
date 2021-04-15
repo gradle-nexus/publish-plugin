9f45565ed3dec03647743f612df5bcd972dcf557
Hidde Wieringa <hidde@hiddewieringa.nl>
2021-04-15 21:20:20 +0200

    And another Spotless style bug

--- src/main/kotlin/io/github/gradlenexus/publishplugin/internal/NexusClient.kt
+++ src/main/kotlin/io/github/gradlenexus/publishplugin/internal/NexusClient.kt
@@ -28,7 +28,6 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
 import java.io.IOException
-import java.io.UncheckedIOException
 import java.net.URI
 import java.time.Duration
 
