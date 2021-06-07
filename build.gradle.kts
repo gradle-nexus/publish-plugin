d3786fa273017ce6fc74181288fa6704893aecf0
Nayan Hajratwala <nhajrat3@ford.com>
2021-06-07 13:41:38 -0400

    updated the "easy to update" dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     `kotlin-dsl`
     `maven-publish`
-    id("com.gradle.plugin-publish") version "0.14.0"
-    id("com.diffplug.spotless") version "5.12.1"
+    id("com.gradle.plugin-publish") version "0.15.0"
+    id("com.diffplug.spotless") version "5.12.5"
     id("com.github.johnrengelman.shadow") version "6.1.0"
     id("org.jetbrains.gradle.plugin.idea-ext")
-    id("com.github.ben-manes.versions") version "0.38.0"
+    id("com.github.ben-manes.versions") version "0.39.0"
     id("org.jetbrains.dokka") version "1.4.32"
     id("org.ajoberstar.stutter") version "0.6.0"
 }
@@ -85,11 +85,11 @@ dependencies {
     shadowed("com.squareup.retrofit2:converter-gson:2.9.0")
     shadowed("net.jodah:failsafe:2.4.0")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
     testImplementation("com.github.tomakehurst:wiremock:2.27.2")
     testImplementation("ru.lanwen.wiremock:wiremock-junit5:1.3.1")
     testImplementation("org.assertj:assertj-core:3.19.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:3.9.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:3.11.0")
     testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
 }
 
