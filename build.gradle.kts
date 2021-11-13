f622f5d6ed3e86dc3bbc4ee12d739548c773a870
Marc Philipp <mail@marcphilipp.de>
2021-11-13 19:58:40 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,13 +82,13 @@ configurations {
 dependencies {
     shadowed("com.squareup.retrofit2:retrofit:2.9.0")
     shadowed("com.squareup.retrofit2:converter-gson:2.9.0")
-    shadowed("net.jodah:failsafe:2.4.0")
+    shadowed("net.jodah:failsafe:2.4.3")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation("com.github.tomakehurst:wiremock:2.27.2")
     testImplementation("ru.lanwen.wiremock:wiremock-junit5:1.3.1")
-    testImplementation("org.assertj:assertj-core:3.19.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:3.11.0")
+    testImplementation("org.assertj:assertj-core:3.21.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:4.0.0")
     testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
 }
 
