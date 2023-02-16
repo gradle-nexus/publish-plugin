46f09434dd27b996b9175ddd066cdb153898b277
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-02-16 10:41:48 +0000

    Lock in Java compilation version to the same as Kotlin, otherwise things can go wrong.
    > Task :compileKotlin FAILED
    
    FAILURE: Build failed with an exception.
    
    * What went wrong:
    Execution failed for task ':compileKotlin'.
    > 'compileJava' task (current target is 11) and 'compileKotlin' task (current target is 1.8) jvm target compatibility should be set to the same Java version.
      Consider using JVM toolchain: https://kotl.in/gradle/jvm/toolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -138,9 +138,14 @@ sourceSets {
     }
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 tasks {
     withType<KotlinCompile>().configureEach {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
     val relocateShadowJar by registering(ConfigureShadowRelocation::class) {
         target = shadowJar.get()
