539aeb20382bbf9b231704eff66b265cd478bc9a
Szava Maczika <smaczika@gradle.com>
2023-05-24 09:26:07 +0200

    Add support for defining the ivy repository pattern layout

--- src/compatTest/kotlin/io/github/gradlenexus/publishplugin/IvyNexusPublishPluginTests.kt
+++ src/compatTest/kotlin/io/github/gradlenexus/publishplugin/IvyNexusPublishPluginTests.kt
@@ -81,6 +81,55 @@ class IvyNexusPublishPluginTests : BaseNexusPublishPluginTests() {
         assertUploaded("/snapshots/org.example/sample/0.0.1-SNAPSHOT/sample-0.0.1-.*.module")
     }
 
+    @Test
+    fun `publishes snapshots with ivy pattern`() {
+        projectDir.resolve("settings.gradle").write(
+            """
+            rootProject.name = 'sample'
+        """
+        )
+
+        projectDir.resolve("build.gradle").write(
+            """
+            plugins {
+                id('java-library')
+                id('${publishPluginId}')
+                id('io.github.gradle-nexus.publish-plugin')
+            }
+            group = 'org.example'
+            version = '0.0.1-SNAPSHOT'
+            publishing {
+                publications {
+                     $publishPluginContent
+                }
+            }
+            nexusPublishing {
+                ivyPatternLayout {
+                    artifact "[organisation]/[module]_foo/[revision]/[artifact]-[revision](-[classifier])(.[ext])"
+                    m2compatible = true
+                }
+                publicationType = io.github.gradlenexus.publishplugin.NexusPublishExtension.PublicationType.${publicationTypeName}
+                repositories {
+                    myNexus {
+                        nexusUrl = uri('${server.baseUrl()}/shouldNotBeUsed')
+                        snapshotRepositoryUrl = uri('${server.baseUrl()}/snapshots')
+                        allowInsecureProtocol = true
+                        username = 'username'
+                        password = 'password'
+                    }
+                }
+            }
+            """
+        )
+
+        expectArtifactUploads("/snapshots")
+
+        val result = run("publishToMyNexus")
+
+        assertSkipped(result, ":initializeMyNexusStagingRepository")
+        assertUploaded("/snapshots/org/example/sample_foo/0.0.1-SNAPSHOT/sample-0.0.1-.*.jar")
+        assertUploaded("/snapshots/org/example/sample_foo/0.0.1-SNAPSHOT/ivy-0.0.1-.*.xml")
+    }
 
 
     @Test
@@ -320,7 +369,6 @@ class IvyNexusPublishPluginTests : BaseNexusPublishPluginTests() {
         assertUploadedToStagingRepo("/org.example/gradle-plugin/0.0.1/ivy-0.0.1.xml")
         assertUploadedToStagingRepo("/org.example.foo/org.example.foo.gradle.plugin/0.0.1/ivy-0.0.1.xml")
     }
-
 
 
 }
--- src/main/kotlin/io/github/gradlenexus/publishplugin/NexusPublishExtension.kt
+++ src/main/kotlin/io/github/gradlenexus/publishplugin/NexusPublishExtension.kt
@@ -20,9 +20,8 @@ import org.gradle.api.Action
 import org.gradle.api.DefaultTask
 import org.gradle.api.NamedDomainObjectFactory
 import org.gradle.api.Project
-import org.gradle.api.artifacts.repositories.IvyArtifactRepository
+import org.gradle.api.artifacts.repositories.IvyPatternRepositoryLayout
 import org.gradle.api.provider.Property
-import org.gradle.api.provider.SetProperty
 import org.gradle.api.publish.Publication
 import org.gradle.api.publish.ivy.IvyPublication
 import org.gradle.api.publish.ivy.tasks.PublishToIvyRepository
@@ -31,7 +30,6 @@ import org.gradle.api.publish.maven.tasks.PublishToMavenRepository
 import org.gradle.kotlin.dsl.container
 import org.gradle.kotlin.dsl.newInstance
 import org.gradle.kotlin.dsl.property
-import org.gradle.kotlin.dsl.setProperty
 import java.time.Duration
 import kotlin.reflect.KClass
 
@@ -78,7 +76,10 @@ open class NexusPublishExtension(project: Project) {
 
     val publicationType: Property<PublicationType> = project.objects.property<PublicationType>().convention(PublicationType.MAVEN)
 
-    val ivyRepositoryLayout: Property<String> = project.objects.property<String>().convention("maven")
+    val ivyPatternLayout: Property<Action<IvyPatternRepositoryLayout>> = project.objects.property<Action<IvyPatternRepositoryLayout>>()
+    fun ivyPatternLayout(action: Action<IvyPatternRepositoryLayout>) {
+        ivyPatternLayout.set(action)
+    }
 
     fun repositories(action: Action<in NexusRepositoryContainer>) = action.execute(repositories)
 
