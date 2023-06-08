4782191f8e9b94de5282fa6059d22d84f1566a3c
Marcin Zajączkowski <148013+szpak@users.noreply.github.com>
2023-06-08 23:42:42 +0200

    Switch back versioning to SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "io.github.gradle-nexus"
-version = "2.0.0-rc-1"
+version = "2.0.0-SNAPSHOT"
 
 val readableName = "Nexus Publish Plugin"
 description = "Gradle Plugin for publishing to Nexus that automates creating, closing, and releasing staging repositories"
