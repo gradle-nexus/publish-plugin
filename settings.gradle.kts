5189711d24de7f70bcbdb95769a2d1d69bc41972
Zongle Wang <wangzongler@gmail.com>
2024-04-07 09:39:19 +0800

    Update settings.gradle.kts
    Co-authored-by: Róbert Papp <papp.robert.s@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,9 +7,7 @@ develocity {
     buildScan {
         termsOfUseUrl = "https://gradle.com/terms-of-service"
         termsOfUseAgree = "yes"
-        // TODO: workaround for https://github.com/gradle/gradle/issues/22879.
-        val isCI = providers.environmentVariable("CI").isPresent
-        publishing.onlyIf { isCI }
+        publishing.onlyIf { System.getenv("CI") != null }
     }
 }
 
